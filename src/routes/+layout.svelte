55c7972095f4ab54efa682e53f9c030b94f50c2d
Sebastian Asprino <sebastian@asprino.dev>
2026-03-08 11:11:55 -0500

    feat(assets): Add comprehensive favicons and web manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,7 +35,15 @@
 	setContext("estadoGlobal", estadoGlobal);
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head
+	><link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="Alcancia" />
+	<link rel="manifest" href="/site.webmanifest" /></svelte:head
+>
 
 <div class="min-h-screen bg-base-200 p-4 md:p-8">
 	{@render children()}
